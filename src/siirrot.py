0afbc8ef81404fd8544834887350ad08d5406df4
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-11-17 21:26:19 +0200

    voittajan etsintä

--- src/siirrot.py
+++ src/siirrot.py
@@ -8,7 +8,7 @@ class Siirto:
     def tarkista_siirto(self, siirto):
         self.virhe = False
         viesti = "Koordinaatti väärässsä muodossa"
-        if len(siirto) != 2:
+        if len(siirto) > 3 or len(siirto) < 2:
             self.virhe = True
             print(viesti)
         elif siirto[0] not in "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst":
@@ -25,17 +25,49 @@ class Siirto:
         while i < 400:
             self.pelilauta.tulosta_ruudukko()
             siirto = input("Valitse koordinaatit (esim. A1):")
-            if self.tarkista_siirto(siirto) == False:
+            if not self.tarkista_siirto(siirto):
                 paikka = self.muuta_koordinaateiksi(siirto)
-                if i % 2 == 0 and self.pelilauta.ruudukko[paikka[1]][paikka[0]] == "-":
-                    self.pelilauta.ruudukko[paikka[1]][paikka[0]] = "X"
-                elif i % 2 != 0 and self.pelilauta.ruudukko[paikka[1]][paikka[0]] == "-":
-                    self.pelilauta.ruudukko[paikka[1]][paikka[0]] = "O"
+                if i % 2 == 0 and self.pelilauta.ruudukko[paikka[0]][paikka[1]] == "-":
+                    self.pelilauta.ruudukko[paikka[0]][paikka[1]] = "X"
+                    print(self.etsi_voittajaa(siirto, "X"))
+                elif i % 2 != 0 and self.pelilauta.ruudukko[paikka[0]][paikka[1]] == "-":
+                    self.pelilauta.ruudukko[paikka[0]][paikka[1]] = "O"
+                    print(self.etsi_voittajaa(siirto, "O"))
                 i += 1
 
-    #def etsi_voittajaa(self, siirto):
-        
+    def etsi_voittajaa(self, siirto, pelaaja):
+        koord = self.muuta_koordinaateiksi(siirto)
+        i = koord[0]
+        j = koord[1]
+        ruudut = self.pelilauta.ruudukko
+        merkkijono1 = ""
+        merkkijono2 = ""
+        merkkijono3 = ""
+        merkkijono4 = ""
+        for x in range(20):
+            merkkijono1 += ruudut[x][j]
+        for y in range(20):
+            merkkijono2 += ruudut[i][y]
+        if self.etsi_rivi(merkkijono1, pelaaja) != -1 or self.etsi_rivi(merkkijono2, pelaaja) != -1:
+            return True
+        diagonaali_i = i - min(i, j)
+        diagonaali_j = j - min(i, j)
+        for k in range(20 - max(diagonaali_i, diagonaali_j)):
+            merkkijono3 += ruudut[diagonaali_i + k][diagonaali_j + k]
+        diagonaali_i = i + min(19 - i, j)
+        diagonaali_j = j - min(19 - i, j)
+        for k in range(20 - max(19 - diagonaali_i, diagonaali_j)):
+            merkkijono4 += ruudut[diagonaali_i - k][diagonaali_j + k]
+            print(diagonaali_i - k, diagonaali_j + k)
+        if self.etsi_rivi(merkkijono3, pelaaja) != -1 or self.etsi_rivi(merkkijono4, pelaaja) != -1:
+            return True
+
+    def etsi_rivi(self, merkit, pelaaja):
+        voitto = pelaaja * 5
+        return merkit.find(voitto)
+
+
     def muuta_koordinaateiksi(self, siirto):
         sarake = ord(siirto[0].lower()) - 97
-        rivi = int(siirto[1]) - 1
-        return (sarake, rivi)
+        rivi = int(siirto[1:]) - 1
+        return (rivi, sarake)
