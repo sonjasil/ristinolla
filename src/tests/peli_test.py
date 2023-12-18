be8893a7cf3a8115ece564ca3e35df80ba5c76de
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-12-18 13:46:09 +0200

    testin korjaus

--- src/tests/peli_test.py
+++ src/tests/peli_test.py
@@ -4,6 +4,7 @@ from siirrot import Peli
 class TestPeli(unittest.TestCase):
     def setUp(self):
         self.peli = Peli()
+        self.ruudukko = self.peli.pelilauta.ruudukko
 
     def test_etsi_voittajaa_vaakarivi(self):
         self.peli.tee_siirto((0, 0), "X")
@@ -12,7 +13,7 @@ class TestPeli(unittest.TestCase):
         self.peli.tee_siirto((0, 3), "X")
         self.peli.tee_siirto((0, 4), "X")
 
-        self.assertEqual(self.peli.etsi_voittajaa((0, 4), "X"), True)
+        self.assertEqual(self.peli.etsi_voittajaa((0, 4), "X", self.ruudukko), True)
 
     def test_etsi_voittajaa_pystyrivi(self):
         self.peli.tee_siirto((1, 3), "X")
@@ -21,7 +22,7 @@ class TestPeli(unittest.TestCase):
         self.peli.tee_siirto((4, 3), "X")
         self.peli.tee_siirto((5, 3), "X")
 
-        self.assertEqual(self.peli.etsi_voittajaa((5, 3), "X"), True)
+        self.assertEqual(self.peli.etsi_voittajaa((5, 3), "X", self.ruudukko), True)
 
     def test_etsi_voittajaa_diagonaali_alas(self):
         self.peli.tee_siirto((0, 0), "X")
@@ -30,7 +31,7 @@ class TestPeli(unittest.TestCase):
         self.peli.tee_siirto((3, 3), "X")
         self.peli.tee_siirto((4, 4), "X")
 
-        self.assertEqual(self.peli.etsi_voittajaa((2, 2), "X"), True)
+        self.assertEqual(self.peli.etsi_voittajaa((2, 2), "X", self.ruudukko), True)
 
     def test_etsi_voittajaa_diagonaali_ylos(self):
         self.peli.tee_siirto((6, 6), "X")
@@ -39,4 +40,4 @@ class TestPeli(unittest.TestCase):
         self.peli.tee_siirto((3, 9), "X")
         self.peli.tee_siirto((2, 10), "X")
 
-        self.assertEqual(self.peli.etsi_voittajaa((3, 9), "X"), True)
+        self.assertEqual(self.peli.etsi_voittajaa((3, 9), "X", self.ruudukko), True)
