0d38342da7778f6fe8e19b53d9c1857c87018c26
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-12-24 15:20:30 +0200

    uusia testejä

--- src/tests/peli_test.py
+++ src/tests/peli_test.py
@@ -41,3 +41,38 @@ class TestPeli(unittest.TestCase):
         self.peli.tee_siirto((2, 10), "X")
 
         self.assertEqual(self.peli.etsi_voittajaa((3, 9), "X", self.ruudukko), True)
+
+    def test_siirron_validointi_vaara_numero(self):
+        self.assertEqual(self.peli.tarkista_siirto("21", "19"), True)
+
+    def test_siirron_validointi_ei_numeroa(self):
+        self.assertEqual(self.peli.tarkista_siirto("a", "b"), True)
+
+    def test_etsi_mahdolliset_siirrot(self):
+        peli = Peli()
+        peli.tee_siirto((5, 5), "X")
+        peli.etsi_mahdolliset_siirrot((5, 5), peli.mahdolliset_siirrot)
+        ymparoivat_siirrot = [(2, 4), (5, 5), (6, 2), (3, 4), (4, 3), (5, 4), (4, 6), (5, 3), (6, 4), (4, 2), (4, 5), (3, 3), (2, 6), (2, 2), (6, 6), (3, 5)]
+
+        self.assertEqual(list(peli.mahdolliset_siirrot).sort(), ymparoivat_siirrot.sort())
+
+    def test_esta_pelaajan_voitto(self):
+        peli = Peli()
+        peli.tee_siirto((4, 4), "X")
+        peli.etsi_mahdolliset_siirrot((4, 4), peli.mahdolliset_siirrot)
+        peli.tee_siirto((2, 4), "O")
+        peli.etsi_mahdolliset_siirrot((2, 4), peli.mahdolliset_siirrot)
+        peli.tee_siirto((4, 5), "X")
+        peli.etsi_mahdolliset_siirrot((4, 5), peli.mahdolliset_siirrot)
+        peli.tee_siirto((3, 4), "O")
+        peli.etsi_mahdolliset_siirrot((3, 4), peli.mahdolliset_siirrot)
+        peli.tee_siirto((4, 6), "X")
+        peli.etsi_mahdolliset_siirrot((4, 6), peli.mahdolliset_siirrot)
+        peli.tee_siirto((4, 3), "O")
+        peli.etsi_mahdolliset_siirrot((4, 3), peli.mahdolliset_siirrot)
+        peli.tee_siirto((4, 7), "X")
+        peli.etsi_mahdolliset_siirrot((4, 7), peli.mahdolliset_siirrot)
+
+        paras_siirto = peli.etsi_paras_siirto(peli.mahdolliset_siirrot, peli.pelilauta)
+
+        self.assertEqual(paras_siirto, (4, 8))
